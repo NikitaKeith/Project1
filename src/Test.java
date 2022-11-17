bda77604763089900cbac8a9418f4093d5eb98bd
Nikita Andreev <keith.sg8@gmail.com>
2022-11-17 19:31:40 +0100

    Add a new Project

---
+++ src/Test.java
@@ -0,0 +1,15 @@
+interface AbleToEat {
+    public void eat();
+}
+
+public class Test {
+    public static void main(String[] args) {
+        AbleToEat ableToEat = new AbleToEat() {
+            @Override
+            public void eat() {
+                System.out.println("eating");
+            }
+        };
+        ableToEat.eat();
+    }
+}
